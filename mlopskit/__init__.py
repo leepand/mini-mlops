587b83ff5a4dfd1d12dd397ccf1b205bfd7f316b
leepand <leepand@leepanddeMacBook-Pro.local>
2023-09-04 18:34:26 +0800

    update

---
+++ mlopskit/__init__.py
@@ -0,0 +1,15 @@
+import warnings
+
+from mlopskit.core.library import ModelLibrary, load_model  # NOQA
+from mlopskit.core.model import Model  # NOQA
+from mlopskit.api import serving
+from mlopskit import pastry
+from mlopskit.pastry.api import make
+from .pipe import Pipe
+
+# Silence Tensorflow warnings
+# https://github.com/tensorflow/tensorflow/issues/30427
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+__version__ = "2.0.2"
