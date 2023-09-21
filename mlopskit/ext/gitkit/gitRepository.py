5d67439a19ae6940d5afdc5b7ace7bb1c5f30d97
leepand <leepand@leepanddeMacBook-Pro.local>
2023-09-21 14:28:41 +0800

    add git utils

--- mlopskit/ext/gitkit/gitRepository.py
+++ mlopskit/ext/gitkit/gitRepository.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import os
 from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
 
 
 from .config import gitconfig_user_get, gitconfig_read
@@ -303,3 +305,169 @@ def cmd_rev_parse(args):
     repo = repo_find()
 
     print(object_find(repo, args.name, fmt, follow=True))
+
+
+def gitignore_parse1(raw):
+    raw = raw.strip()  # Remove leading/trailing spaces
+
+    if not raw or raw[0] == "#":
+        return None
+    elif raw[0] == "!":
+        return (raw[1:], False)
+    elif raw[0] == "\\":
+        return (raw[1:], True)
+    else:
+        return (raw, True)
+
+
+def gitignore_parse(lines):
+    ret = list()
+
+    for line in lines:
+        parsed = gitignore_parse1(line)
+        if parsed:
+            ret.append(parsed)
+
+    return ret
+
+
+class GitIgnore(object):
+    absolute = None
+    scoped = None
+
+    def __init__(self, absolute, scoped):
+        self.absolute = absolute
+        self.scoped = scoped
+
+
+def gitignore_read(repo):
+    ret = GitIgnore(absolute=list(), scoped=dict())
+
+    # Read local configuration in .git/info/exclude
+    repo_file = os.path.join(repo.gitdir, "info/exclude")
+    if os.path.exists(repo_file):
+        with open(repo_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    # Global configuration
+    if "XDG_CONFIG_HOME" in os.environ:
+        config_home = os.environ["XDG_CONFIG_HOME"]
+    else:
+        config_home = os.path.expanduser("~/.config")
+    global_file = os.path.join(config_home, "git/ignore")
+
+    if os.path.exists(global_file):
+        with open(global_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    # .gitignore files in the index
+    index = index_read(repo)
+
+    for entry in index.entries:
+        if entry.name == ".gitignore" or entry.name.endswith("/.gitignore"):
+            dir_name = os.path.dirname(entry.name)
+            contents = object_read(repo, entry.sha)
+            lines = contents.blobdata.decode("utf8").splitlines()
+            ret.scoped[dir_name] = gitignore_parse(lines)
+    return ret
+
+
+def check_ignore1(rules, path):
+    result = None
+    for (pattern, value) in rules:
+        if fnmatch(path, pattern):
+            result = value
+    return result
+
+
+def check_ignore_absolute(rules, path):
+    parent = os.path.dirname(path)
+    for ruleset in rules:
+        result = check_ignore1(ruleset, path)
+        if result != None:
+            return result
+    return False  # This is a reasonable default at this point.
+
+
+def check_ignore_scoped(rules, path):
+    parent = os.path.dirname(path)
+    while True:
+        if parent in rules:
+            result = check_ignore1(rules[parent], path)
+            if result != None:
+                return result
+        if parent == "":
+            break
+        parent = os.path.dirname(parent)
+    return None
+
+
+def check_ignore(rules, path):
+    if os.path.isabs(path):
+        raise Exception(
+            "This function requires path to be relative to the repository's root"
+        )
+
+    result = check_ignore_scoped(rules.scoped, path)
+    if result != None:
+        return result
+
+    return check_ignore_absolute(rules.absolute, path)
+
+
+def cmd_check_ignore(args):
+    repo = repo_find()
+    rules = gitignore_read(repo)
+    for path in args.path:
+        if check_ignore(rules, path):
+            print(path)
+
+
+def cmd_ls_files(verbose=True):
+    repo = repo_find()
+    index = index_read(repo)
+    commit_files = []
+    if verbose:
+        print(
+            "Index file format v{}, containing {} entries.".format(
+                index.version, len(index.entries)
+            )
+        )
+
+    for e in index.entries:
+        print(e.name)
+        commit_files.append(e.name)
+        if verbose:
+            print(
+                "  {} with perms: {:o}".format(
+                    {0b1000: "regular file", 0b1010: "symlink", 0b1110: "git link"}[
+                        e.mode_type
+                    ],
+                    e.mode_perms,
+                )
+            )
+            print("  on blob: {}".format(e.sha))
+            print(
+                "  created: {}.{}, modified: {}.{}".format(
+                    datetime.fromtimestamp(e.ctime[0]),
+                    e.ctime[1],
+                    datetime.fromtimestamp(e.mtime[0]),
+                    e.mtime[1],
+                )
+            )
+            print("  device: {}, inode: {}".format(e.dev, e.ino))
+            print(
+                "  user: {} ({})  group: {} ({})".format(
+                    pwd.getpwuid(e.uid).pw_name,
+                    e.uid,
+                    grp.getgrgid(e.gid).gr_name,
+                    e.gid,
+                )
+            )
+            print(
+                "  flags: stage={} assume_valid={}".format(
+                    e.flag_stage, e.flag_assume_valid
+                )
+            )
+
+    return commit_files
